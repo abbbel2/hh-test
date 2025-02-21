671d03ce98648975403cc9bdf66e934117bc864b
abbbel2 <a.teka@altexsoft.com>
2025-02-21 09:17:39 +0300

    updates

--- web/src/redux/axios.ts
+++ web/src/redux/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://159.89.15.199:3000",
 });
 
 export default axiosInstance;
